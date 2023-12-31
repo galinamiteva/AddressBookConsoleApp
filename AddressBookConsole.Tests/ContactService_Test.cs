7eb642b2bc9eb70c5510e31ccb25ec3a00ffd9a8
Gala <70331948+galinamiteva@users.noreply.github.com>
2023-12-31 18:09:40 +0100

    Tests

---
+++ AddressBookConsole.Tests/ContactService_Test.cs
@@ -0,0 +1,168 @@
+﻿
+using AddressBook.Shared.Interfaces;
+using AddressBook.Shared.Models;
+using AddressBook.Shared.Services;
+
+namespace AddressBookConsole.Tests;
+
+public class ContactService_Test
+{
+    [Fact]  
+    public void AddToListShould_AddOneContactToContactList_ThenReturnTrue()
+    {
+        //Arrange
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        };
+        IContactService contactService = new ContactService();
+       
+        //Act
+        bool result = contactService.AddContactToList(contact);
+
+        //Assert
+        Assert.True(result);
+    }
+
+
+
+    [Fact]
+
+    public void GetAllFromListShould_GetAllContactsInContactList_ThenReturnListOfContact()
+    {
+        //Arrange
+        
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        };
+        IContactService contactService = new ContactService();
+
+        //Act
+        contactService.AddContactToList(contact);
+        var contactList = contactService.GetAllContactsFromList();  
+
+        //Assert
+        Assert.NotNull(contactList);
+
+    }
+
+    [Fact]  
+
+    public void GetOneContactByEmail_ShouldGetOneContactFromList_ThenReturnOneContact()
+    {
+        //Arrange
+
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        };
+        IContactService contactService = new ContactService();
+
+        //Act
+
+        contactService.AddContactToList(contact);   
+        var oneContactFromList = contactService.GetOneContact(contact.Email);   
+
+        //Assert
+        Assert.NotNull(oneContactFromList); 
+    }
+
+
+
+    [Fact]  
+    public void DeleteOneContactByEmail_ShouldDeleteOneContactFromList_AndReturnTrue() 
+    {
+        //Arrange
+
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        };
+        IContactService contactService = new ContactService();
+
+        //Act
+
+        contactService.AddContactToList(contact);
+        var deleteOneContactByEmail = contactService.DeleteContactFromList(contact.Email);
+        var updatedContactList= contactService.GetAllContactsFromList();
+
+        //Assert
+        Assert.True(deleteOneContactByEmail);
+        Assert.DoesNotContain(updatedContactList, x=>x.Email==contact.Email);
+    }
+
+    [Fact]
+    public void UpdateOneContactInList_ShouldUpdateContact_ReturnTrue() 
+    {
+        //Arrange
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        };
+        IContactService contactService = new ContactService();
+
+
+
+        //Act
+
+        contactService.AddContactToList(contact);
+        var updatedContact = contactService.UpdateContactInList(contact.Email, "Dobbe", "Mitev", "777777", "Furutorpsgatan", "52", "22222", "Malmo", "Sverige");
+        var updatedContactList = contactService.GetAllContactsFromList();
+        
+        //Assert
+        Assert.True(updatedContact); 
+        Assert.DoesNotContain(updatedContactList, x=>x.Email==contact.Email);
+        Assert.Contains(updatedContactList, x => x.Email == "aaa@com");
+
+
+
+
+
+    }
+
+
+}
+
+
+
---
+++ AddressBookConsole.Tests/FileService_Test.cs
@@ -0,0 +1,82 @@
+﻿
+
+using AddressBook.Shared.Interfaces;
+using AddressBook.Shared.Models;
+using AddressBook.Shared.Services;
+using Newtonsoft.Json;
+
+namespace AddressBookConsole.Tests;
+
+public class FileService_Test
+{
+    [Fact]
+
+    public void SaveToFileShould_ReturnFalse_IfFilePathDoNotExists()
+    {
+        //Arrange
+
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        };
+        IContactService contactService = new ContactService();
+        FileService fileService = new();     
+
+
+        //Act
+
+        contactService.AddContactToList(contact);
+        var contactList = contactService.GetAllContactsFromList();
+        string json = JsonConvert.SerializeObject(contactList, new JsonSerializerSettings { TypeNameHandling=TypeNameHandling.All});
+        var result = fileService.SaveContentToFile(json);
+
+        //Assert
+
+        Assert.True(result);
+    }
+
+    [Fact]  
+    public void GetContentFromFile_ShouldGetContentFromFile_ReturnContent() 
+    {
+        //Arrange
+        IContact contact = new Contact
+        {
+            FirstName = "Alexandra",
+            LastName = "Miteva",
+            Email = "aaa@com",
+            PhoneNumber = "123456",
+            Street = "Furutorpsgatan",
+            StreetNumber = "52",
+            PostalCode = "25255",
+            City = "Helsingborg",
+            Country = "Sverige"
+        }; 
+        IContactService contactService = new ContactService(); 
+        FileService fileService = new();
+
+
+        //Act
+
+        contactService.AddContactToList(contact);
+        var contactList = contactService.GetAllContactsFromList();
+        string json = JsonConvert.SerializeObject(contactList, new JsonSerializerSettings { TypeNameHandling = TypeNameHandling.All });
+        var result= fileService.SaveContentToFile(json);
+
+        var content = fileService.GetContentFromFile();
+        contactList = JsonConvert.DeserializeObject<List<IContact>>(content, new JsonSerializerSettings { TypeNameHandling= TypeNameHandling.All});
+
+
+        //Assert
+        Assert.Equal(json, content);
+        Assert.True(result);
+
+    }
+}
